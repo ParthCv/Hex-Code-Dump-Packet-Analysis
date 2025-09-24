f86c106ee8d2b19726bedc65445d2a0d6d4fada0
ParthCv <parthcv0101@gmail.com>
2025-09-23 19:17:47 -0700

    Add some IPv4 handling

--- packet_parsers.py
+++ packet_parsers.py
@@ -14,6 +14,8 @@ def parse_ethernet_header(hex_data):
     # Route payload based on EtherType
     if ether_type == "0806":  # ARP
         parse_arp_header(payload)
+    elif ether_type == "0800": # IPv4
+        parse_ipv4_header(payload)
     else:
         print(f"  {'Unknown EtherType:':<25} {ether_type:<20} | {int(ether_type, 16)}")
         print("  No parser available for this EtherType.")
@@ -51,3 +53,30 @@ def parse_arp_header(hex_data):
     print(f"  {'Target Mac address:':<25} {target_mac_hex:<20} | {target_mac}")
     print(f"  {'Target IP address:':<25} {target_ip_hex:<20} | {target_ip}")
     print(f"")
+
+def parse_ipv4_header(hex_data):
+    version = int(hex_data[0], 16)
+    header_length = int(hex_data[1], 16) * 4
+    total_length = int(hex_data[4:8], 16)
+    identification =  int(hex_data[8:12], 16)
+    flags_and_fragment = int(hex_data[12:16], 16)
+    # add the reserved, df, mf and fragment offset flags
+
+    protocol = int(hex_data[18:20], 16)
+
+    source_ip_hex = hex_data[24:32]
+    source_ip_ints_list = [str(int(source_ip_hex[i:i+2], 16)) for i in range(0, 8, 2)]
+    source_ip = '.'.join(source_ip_ints_list)
+
+    destination_ip_hex = hex_data[32:40]
+    destination_ip_ints_list = [str(int(destination_ip_hex[i:i+2], 16)) for i in range(0, 8, 2)]
+    destination_ip = '.'.join(destination_ip_ints_list)
+
+    print(f"IPv4 Header:")
+    print(f"  {'Version:':<25} {hex_data[0]:<20} | {version}")
+    print(f"  {'Header Length:':<25} {hex_data[1]:<20} | {header_length}")
+    print(f"  {'Total Length:':<25} {hex_data[4:8]:<20} | {total_length}")
+    print(f"  {'Flags & Frag Offset:':<25} {hex_data[12:16]:<20} | {flags_and_fragment}")
+    print(f"  {'Protocol:':<25} {hex_data[18:20]:<20} | {protocol}")
+    print(f"  {'Source IP:':<25} {hex_data[24:32]:<20} | {source_ip}")
+    print(f"  {'Destination IP:':<25} {hex_data[32:40]:<20} | {destination_ip}")
